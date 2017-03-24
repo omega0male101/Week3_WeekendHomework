d7a7ae3867f0bee0f4847fdf33b17c6794e003d9
Eden <edenstevens101@gmail.com>
2017-03-24 11:19:19 +0000

    Created Class for Customers

---
+++ models/customer.rb
@@ -0,0 +1,15 @@
+require('pg')
+require_relative ("../db/sql_runner")
+require_relative('ticket')
+
+class Customer
+
+  attr_reader :name, :funds, :id
+
+  def initialize(options)
+    @name = options["name"]
+    @funds = options["funds"].to_i
+    @id = options["id"].to_i
+  end
+
+end

9a76e536d07bf1d2751fba8d9173d506e3e4e5b0
Eden <edenstevens101@gmail.com>
2017-03-24 11:08:25 +0000

    DB name change on SQL  _Runner

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -2,7 +2,7 @@ class SqlRunner
 
   def self.run(sql)
     begin
-      db = PG.connect( {dbname: 'music_app', host: 'localhost'} )
+      db = PG.connect( {dbname: 'cinema', host: 'localhost'} )
       result = db.exec(sql)
     ensure
       db.close()
